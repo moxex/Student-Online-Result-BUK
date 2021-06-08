9ee1ad1e8e9cbd71d2d6e562544a8135ee5f30f2
Moses Eyime Akoji <31869504+moxex@users.noreply.github.com>
2021-06-08 22:53:41 +0100

    Updated error

--- create-subject.php
+++ create-subject.php
@@ -105,13 +105,13 @@ else if($error){?>
                                                     <div class="form-group">
                                                         <label for="default" class="col-sm-2 control-label">Course Title</label>
                                                         <div class="col-sm-10">
- <input type="text" name="subjectname" class="form-control" id="default" placeholder="Subject Name" required="required">
+ <input type="text" name="subjectname" class="form-control" id="default" placeholder="Course Title" required="required">
                                                         </div>
                                                     </div>
 <div class="form-group">
                                                         <label for="default" class="col-sm-2 control-label">Course Code</label>
                                                         <div class="col-sm-10">
- <input type="text" name="subjectcode" class="form-control" id="default" placeholder="Subject Code" required="required">
+ <input type="text" name="subjectcode" class="form-control" id="default" placeholder="Course Code" required="required">
                                                         </div>
                                                     </div>
                                                     
